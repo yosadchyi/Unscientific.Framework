c35971348b7cb1545e5bf507be1dd37a3f19b243
yosadchyi <yurii.osadchyi@gmail.com>
2018-03-29 11:45:20 +0300

    [ECS] Add PeriodicUpdateSystem.

---
+++ Unscientific.ECS/Sources/System/PeriodicUpdateSystem.cs
@@ -0,0 +1,25 @@
+﻿namespace Unscientific.ECS
+{
+    public class PeriodicUpdateSystem: IUpdateSystem
+    {
+        private IUpdateSystem _decordatedSystem;
+        private readonly int _period;
+        private int _counter;
+
+        public PeriodicUpdateSystem(IUpdateSystem decordatedSystem, int period)
+        {
+            _decordatedSystem = decordatedSystem;
+            _period = period;
+        }
+
+        public void Update()
+        {
+            _counter++;
+            if (_counter % _period == 0)
+            {
+                _decordatedSystem.Update();
+                _counter = 0; // avoid overflow
+            }
+        }
+    }
+}
