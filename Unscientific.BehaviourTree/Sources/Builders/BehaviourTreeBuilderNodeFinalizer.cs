20dd43bda3b7760f62e2baba1a7a0800ddea1b39
yurii.osadchyi@gmail.com <yurii_osadchyi@epam.com>
2018-05-02 11:17:31 +0300

    [BehaviourTree] Builders refactoring.

---
+++ Unscientific.BehaviourTree/Sources/Builders/BehaviourTreeBuilderNodeFinalizer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Unscientific.BehaviourTree
+{
+    public class BehaviourTreeBuilderNodeFinalizer<TBlackboard, TParent> where TParent: INodeAcceptor<TBlackboard>
+    {
+        private readonly TParent _parent;
+        private readonly BehaviourTreeNode<TBlackboard> _node;
+
+        public BehaviourTreeBuilderNodeFinalizer(TParent parent, BehaviourTreeNode<TBlackboard> node)
+        {
+            _parent = parent;
+            _node = node;
+        }
+
+        public TParent End()
+        {
+            _parent.AcceptNode(_node);
+            return _parent;
+        }
+    }
+}
