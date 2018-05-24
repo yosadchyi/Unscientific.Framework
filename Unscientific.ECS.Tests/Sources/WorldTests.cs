82bcee28de7ae5a3c4ca3875b50897b5635f9627
Yurii Osadchyi <yurii.osadchyi@gmail.com>
2018-05-24 16:23:12 +0300

    [ECS] Added test for system execution order.

---
+++ Unscientific.ECS.Tests/Sources/WorldTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Unscientific.ECS.DSL;
+
+namespace Unscientific.ECS.Tests
+{
+    [TestFixture]
+    public class WorldTests
+    {
+        [Test]
+        public void TestThatSystemsAreExecutedInProperOrder()
+        {
+            var log = new List<string>();
+            // @formatter:off
+            var world = new WorldBuilder()
+                .AddFeature("Feature 1")
+                    .Systems()
+                        .Setup(((contexts, bus) => log.Add("F1-S1")))
+                        .Update(((contexts, bus) => log.Add("F1-U1")))
+                        .Cleanup(((contexts, bus) => log.Add("F1-C1")))
+                    .End()
+                .End()
+                .AddFeature("Feature 2")
+                    .Systems()
+                        .Setup(((contexts, bus) => log.Add("F2-S1")))
+                        .Update(((contexts, bus) => log.Add("F2-U1")))
+                        .Cleanup(((contexts, bus) => log.Add("F2-C1")))
+                        .Cleanup(((contexts, bus) => log.Add("F2-C2")))
+                    .End()
+                .End()
+                .AddFeature("Feature 3")
+                    .Systems()
+                        .Setup(((contexts, bus) => log.Add("F3-S1")))
+                        .Update(((contexts, bus) => log.Add("F3-U1")))
+                        .Cleanup(((contexts, bus) => log.Add("F3-C1")))
+                    .End()
+                .End()
+            .Build();
+            // @formatter:on
+
+            world.Setup();
+            world.Update();
+            world.Cleanup();
+
+            CollectionAssert.AreEqual(new[] {
+                "F1-S1", "F2-S1", "F3-S1",
+                "F1-U1", "F2-U1", "F3-U1",
+                "F3-C1", "F2-C1", "F2-C2", "F1-C1"}, log);
+        }
+    }
+}
