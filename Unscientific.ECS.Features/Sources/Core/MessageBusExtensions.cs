6d1666b6c8044b8a076d1b1fde00c7b8eb04ca65
Yurii Osadchyi <yurii.osadchyi@gmail.com>
2018-06-08 17:03:55 +0300

    [Core] Added MessageBusExtensions to support component notifications in Systems.

---
+++ Unscientific.ECS.Features/Sources/Core/MessageBusExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Unscientific.ECS.Features.Core
+{
+    public static class MessageBusExtensions
+    {
+        public static MessageEnumerable<ComponentAdded<TScope, TComponent>> AllComponentAdded<TScope, TComponent>(this MessageBus bus)
+        {
+            return bus.All<ComponentAdded<TScope, TComponent>>();
+        } 
+        
+        public static MessageEnumerable<ComponentRemoved<TScope, TComponent>> AllComponentRemoved<TScope, TComponent>(this MessageBus bus)
+        {
+            return bus.All<ComponentRemoved<TScope, TComponent>>();
+        } 
+        
+        public static MessageEnumerable<ComponentReplaced<TScope, TComponent>> AllComponentReplaced<TScope, TComponent>(this MessageBus bus)
+        {
+            return bus.All<ComponentReplaced<TScope, TComponent>>();
+        }
+    }
+}
